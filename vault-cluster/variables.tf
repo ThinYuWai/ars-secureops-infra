ae574d06afa648dc7dfb763eff51cea5a2aa3b76
ThinYu-Fundit <thin@fundit.jp>
2024-09-18 15:50:52 +0900

    remove client id and client secrete variables

--- vault-cluster/variables.tf
+++ vault-cluster/variables.tf
@@ -3,13 +3,3 @@ variable "aws_region" {
   description = "AWS Region ap-southeast-6 for vault-cluster"
   default     = "ap-southeast-1"
 }
-
-variable "HCP_CLIENT_SECRET" {
-  description = "The secret for the HCP client."
-  type        = string
-}
-
-variable "HCP_CLIENT_ID" {
-  description = "The ID for the HCP client."
-  type        = string
-}
